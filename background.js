9aa28ac7592b56d369538bb19f8ef6068397c16c
Tony Colston <tonetheman@gmail.com>
2017-10-23 19:23:05 -0500

    added external message handler

--- background.js
+++ background.js
@@ -15,6 +15,22 @@ chrome.runtime.onMessage.addListener(
       sendResponse({farewell: "goodbye"});
   });
 
+  // external messages come from
+  // web pages
+  chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+
+      console.log("TONY EXTERNAL MESSAGE IN BACKGROUND:",request,sender);
+      if (request.getTargetData) {
+        console.log("need to open tab now");
+        chrome.browser.openTab({
+          url: "http://google.com"
+        });
+      }
+
+
+    });
+
 
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
     chrome.app.window.create("tony.html");
