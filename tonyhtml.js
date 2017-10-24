390ef10a5682a0d8807b3fd09e048e306d52daa0
Tony Colston <tonetheman@gmail.com>
2017-10-23 19:26:03 -0500

    removing content script mention

--- tonyhtml.js
+++ tonyhtml.js
@@ -9,16 +9,6 @@ window.onload = function() {
   });
 
   /*
-  wv.addContentScripts([
-    {
-      name : "testclick",
-      matches : [ "https://crossbrowsertesting.com/bookmarklet/bookmarkletchromeext.php"],
-      js :  { files: ["tonycontentscript.js"] },
-      run_at: "document_start"
-    }
-  ]);
-  */
-  /*
   // this works when run here
   var id = "gihefhgcfelceogbkmhcffhlnadljppe";
   chrome.runtime.sendMessage(id,
